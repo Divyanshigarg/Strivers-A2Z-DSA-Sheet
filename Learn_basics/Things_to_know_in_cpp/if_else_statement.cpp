0bce2c4c6bc311a034a0ee913d3fdf5ce11d1928
Divyanshigarg <gargdivyanshi27@gmail.com>
2022-10-04 09:55:24 +0530

    solutions

---
+++ Learn_basics/Things_to_know_in_cpp/if_else_statement.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    string compareNM(int n, int m){
+        // code here
+        if(n<m)
+        cout<<"lesser";
+        else if(n==m)
+        cout<<"equal";
+        else 
+        cout<<"greater";
+    }
+};
