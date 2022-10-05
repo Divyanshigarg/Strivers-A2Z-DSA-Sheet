2e55d6bba179798847a3d6bf789c08e47ee44ca3
Divyanshigarg <gargdivyanshi27@gmail.com>
2022-10-05 12:00:51 +0530

    solution

---
+++ Learn_basics/Things_to_know_in_cpp/while_loop.cpp
@@ -0,0 +1,13 @@
+class Geeks {
+	 static void printTable (int n) 
+	 {
+	     int res=0;
+            int multiplier=10;
+            while(multiplier>0)
+            {
+                   System.out.print(multiplier*n+" ");
+                multiplier--;
+            }
+        System.out.println();
+	 }
+}
