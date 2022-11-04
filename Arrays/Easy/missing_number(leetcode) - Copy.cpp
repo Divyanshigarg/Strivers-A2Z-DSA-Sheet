82119043d79575bd0abd383c37f709b80b499f09
Divyanshigarg <gargdivyanshi27@gmail.com>
2022-11-04 17:11:43 +0530

    easy

---
+++ Arrays/Easy/missing_number(leetcode) - Copy.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int total_sum=(n*(n+1))/2;
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+        }
+        return total_sum-sum;
+    }
+};
